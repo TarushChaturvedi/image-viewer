f95ad33ec1162587b2f5b215c0970876f069ccd3
Tarush Chaturvedi <tarush.chaturvedi@gmail.com>
2024-05-09 05:14:12 -0500

    Add LICENSE and clean up includes

--- src/main.c
+++ src/main.c
@@ -1,10 +1,5 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_surface.h>
-#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdbool.h>
 
